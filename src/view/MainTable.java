13fce227d89615ec8cd519522b7cb496464b4e12
Rizhi-KotE <d1mqn1ch@mail.ru>
2016-03-31 10:27:47 +0300

    universal table

--- src/view/MainTable.java
+++ src/view/MainTable.java
@@ -1,21 +1,18 @@
 package view;
 
-import java.lang.reflect.Field;
-
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import model.Book;
 import model.Library;
-import model.NamedFields;
 
-public class MainTable<T> {
-	private TableView<T> table;
+public class MainTable {
+	private TableView table;
 	private Library library;
 	/**
 	 * @return the table
 	 */
-	public TableView<T> getTable() {
+	public TableView getTable() {
 		return table;
 	}
 
@@ -26,27 +23,25 @@ public class MainTable<T> {
 		table.setEditable(true);
 	}
 
-	private TableView<T extends NamedFields> initTable(TableView<T> table) {
-		Field[] fields = 
-		
-		TableColumn<T, String> bookNameCol = new TableColumn<>("Book's name");
+	private TableView<Book> initTable(TableView<Book> table) {
+		TableColumn<Book, String> bookNameCol = new TableColumn<>("Book's name");
 		bookNameCol.setMinWidth(100);
 		bookNameCol.setCellValueFactory(new PropertyValueFactory<Book, String>("bookName"));
 		
 		
-		TableColumn<T, String> authorCol = new TableColumn<Book, String>("author");
+		TableColumn<Book, String> authorCol = new TableColumn<Book, String>("author");
 		authorCol.setMinWidth(100);
 		authorCol.setCellValueFactory(new PropertyValueFactory<Book, String>("authorName"));
 
-		TableColumn<T, String> circulationCol = new TableColumn<Book, String>("circulation");
+		TableColumn<Book, String> circulationCol = new TableColumn<Book, String>("circulation");
 		circulationCol.setMinWidth(100);
 		circulationCol.setCellValueFactory(new PropertyValueFactory<Book, String>("circulation"));
 
-		TableColumn<T, String> tomesNumCol = new TableColumn<Book, String>("amount of tomes");
+		TableColumn<Book, String> tomesNumCol = new TableColumn<Book, String>("amount of tomes");
 		tomesNumCol.setMinWidth(100);
 		tomesNumCol.setCellValueFactory(new PropertyValueFactory<Book, String>("tomeNumber"));
 
-		TableColumn<T, String> finalTomesNumCol = new TableColumn<Book, String>("final amount of tomes");
+		TableColumn<Book, String> finalTomesNumCol = new TableColumn<Book, String>("final amount of tomes");
 		finalTomesNumCol.setMinWidth(100);
 		finalTomesNumCol.setCellValueFactory(new PropertyValueFactory<Book, String>("finalTomeNumber"));
 		
